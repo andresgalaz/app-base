b6031a4df9347e73479d5068d44faf74fd508894
Andres Galaz <andres.galaz@gmail.com>
2018-08-09 16:25:30 -0300

    main al inicio

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 /*
- * This file launches the application by asking Ext JS to create
- * and launch() the Application class.
+ * PE Aplicación para Móviles
  */
 Ext.application({
     extend: 'a2m.Application',
-
     name: 'a2m',
-
     requires: [
-        // This will automatically load all classes in the a2m namespace
-        // so that application classes do not need to require each other.
         'a2m.*', 'Ext.grid.Grid', 'Ext.data.JsonStore', 'Ext.scroll.Scroller', 'Ext.field.*', 'Ext.util.Base64'
     ],
+    mainView: 'a2m.view.main.Main'
+
 });
